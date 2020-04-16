eec31e4f24fd32756bdaa15d0c275b8b7197da9f
viveklambhi1 <vivek.lambhi@splashmath.com>
2020-04-17 01:58:59 +0530

    space remove

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-
 app.get('/home', function(req,res){
   res.status(200).json("its working");
 })
